8cbcba9888856ad365cc1c3796de59381341e2bc
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-08 19:50:34 +0300

    badges and schedules complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,55 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
-def batch_badge_creator(names):
-    return None
+name = "Guido van Rossum"
+print(badge_maker(name))
+
+
+def batch_badge_creator(names):    
+    
+    # Using for loop
+    badges = []
+    for name in names:
+        badge = badge_maker(name)
+        badges.append(badge)
+    return badges
+    
+    
+    # Using list comprehensions
+    badges = [badge_maker(name) for name in names]
+    
+    return badges
+
+names = ['Guido', 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+print(batch_badge_creator(names))
+
 
 def assign_rooms(names):
-    return None
+    index = 1
+    messages = []    
+    for name in names:
+        # print(index)
+        message = f"Hello, {name}! You'll be assigned to room {index}!"
+        messages.append(message)
+        index +=1
+        
+    return messages
+
+
+names = ['Guido', 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+print(assign_rooms(names))
 
 def printer(names):
-    return None
+    
+    output1 = batch_badge_creator(names)
+    for message in output1:
+        print(message)
+        
+    output2 = assign_rooms(names)
+    for message2 in output2:
+        print(message2)
+    
+
+names = ['Guido', 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+    
+printer(names)
